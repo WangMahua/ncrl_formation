e59be3af599d30def822e87d2cb489bee672cf64
AMNshaw <75787424+AMNshaw@users.noreply.github.com>
2023-05-04 13:57:50 +0800

    Update gps_init.py

--- src/gps_init.py
+++ src/gps_init.py
@@ -5,6 +5,7 @@ from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import NavSatFix
 from std_msgs.msg import Int32
 import pymap3d as pm
+from mavros_msgs.srv import StreamRate
 
 latO = 0
 lonO = 0
@@ -48,27 +49,28 @@ def pose_init_cb(msg):
 
 if __name__ == '__main__':
 	
-	rospy.init_node('gps_init_py')
-	pose_init_sub = rospy.Subscriber('/uav_init', Int32, pose_init_cb)
-	gps_origin_sub = rospy.Subscriber('/MAV6/mavros/global_position/global', NavSatFix, gps_origin_cb)
-	gps_self_sub = rospy.Subscriber('mavros/global_position/global', NavSatFix, gps_self_cb)
-	gps_pose_sub = rospy.Subscriber('mavros/local_position/pose', PoseStamped, gps_pose_cb)
+        rospy.init_node('gps_init_py')
+        pose_init_sub = rospy.Subscriber('/uav_init', Int32, pose_init_cb)
+        gps_origin_sub = rospy.Subscriber('/MAV6/mavros/global_position/global', NavSatFix, gps_origin_cb)
+        gps_self_sub = rospy.Subscriber('mavros/global_position/global', NavSatFix, gps_self_cb)
+        gps_pose_sub = rospy.Subscriber('mavros/local_position/pose', PoseStamped, gps_pose_cb)
 
-	gps_pose_pub = rospy.Publisher('mavros/local_position/pose_initialized', PoseStamped, queue_size=10)	
+        gps_pose_pub = rospy.Publisher('mavros/local_position/pose_initialized', PoseStamped, queue_size=10)
+        
+        rospy.wait_for_service('mavros/set_stream_rate')
 
-	rate = rospy.Rate(100)
+        stream_rate_client = rospy.ServiceProxy('mavros/set_stream_rate', StreamRate)
+        rospy.loginfo("gps_init node constructed")
+        rate = rospy.Rate(100)
+        while not rospy.is_shutdown():
+            response = stream_rate_client(0, 30, 1)
+            gps_pose_initialized.pose.position.x = gps_pose.pose.position.x - gps_pose_init.pose.position.x + pose_relative.pose.position.x
+            gps_pose_initialized.pose.position.y = gps_pose.pose.position.y - gps_pose_init.pose.position.y + pose_relative.pose.position.y
+            gps_pose_initialized.pose.position.z = gps_pose.pose.position.z - gps_pose_init.pose.position.z 
+            gps_pose_initialized.pose.orientation = gps_pose.pose.orientation
 
-	rospy.loginfo("gps_init node constructed")
+            gps_pose_pub.publish(gps_pose_initialized)
 
-	while not rospy.is_shutdown():
 
-		gps_pose_initialized.pose.position.x = gps_pose.pose.position.x - gps_pose_init.pose.position.x + pose_relative.pose.position.x
-		gps_pose_initialized.pose.position.y = gps_pose.pose.position.y - gps_pose_init.pose.position.y + pose_relative.pose.position.y
-		gps_pose_initialized.pose.position.z = gps_pose.pose.position.z - gps_pose_init.pose.position.z
-		gps_pose_initialized.pose.orientation = gps_pose.pose.orientation
-
-		gps_pose_pub.publish(gps_pose_initialized)
-
-		
-		rate.sleep()
-	rospy.spin()
+            rate.sleep()
+        rospy.spin()
