96af707aef94038d949ad72b875ebb405e85866d
AMNshaw <75787424+AMNshaw@users.noreply.github.com>
2023-07-07 17:11:57 +0800

    Update servo_GPIO.py

--- src/servo_GPIO.py
+++ src/servo_GPIO.py
@@ -1,4 +1,6 @@
-import RPi.GPIO as GPIO
+#!/usr/bin/env python3
+
+import pigpio
 import time
 import rospy
 from std_msgs.msg import Int32
@@ -11,19 +13,16 @@ fire = 0
 
 def fire_cb(msg):
     global fire
-    fire = msg
-    pwm = GPIO.PWM(servo_pin, 1420)
-    pwm.start(0)
-    rospy.loginfo("This is an informational message.")
+    fire = msg.data
+    if fire == 1:
+        rospy.loginfo("Fire")
+        pi.set_servo_pulsewidth(servo_pin, 1420)
+        time.sleep(1)
+        pi.set_servo_pulsewidth(servo_pin, 1050)
 
 if __name__ == '__main__':
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(servo_pin, GPIO.OUT)
     rospy.init_node('gps_init_py')
     fire_sub = rospy.Subscriber('/fire', Int32, fire_cb)
 
     # Create a PWM object with a frequency of 50Hz
-    pwm = GPIO.PWM(servo_pin, 1050)
-    pwm.start(0)
-
     rospy.spin()
