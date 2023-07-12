1282feb175bda2be48d627709e9a586d9affba86
AMNshaw <75787424+AMNshaw@users.noreply.github.com>
2023-07-12 14:25:54 +0800

    Update servo_GPIO.py

--- src/servo_GPIO.py
+++ src/servo_GPIO.py
@@ -16,13 +16,13 @@ def fire_cb(msg):
     fire = msg.data
     if fire == 1:
         rospy.loginfo("Fire")
-        pi.set_servo_pulsewidth(servo_pin, 1420)
-        time.sleep(1)
         pi.set_servo_pulsewidth(servo_pin, 1050)
+        time.sleep(1)
+        pi.set_servo_pulsewidth(servo_pin, 1420)
 
 if __name__ == '__main__':
     rospy.init_node('gps_init_py')
     fire_sub = rospy.Subscriber('/fire', Int32, fire_cb)
-
-    # Create a PWM object with a frequency of 50Hz
+    pi.set_servo_pulsewidth(servo_pin, 1420)
+   
     rospy.spin()
